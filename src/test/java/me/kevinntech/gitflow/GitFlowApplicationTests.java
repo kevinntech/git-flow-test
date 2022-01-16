7ba310ad02b82845b5886da9aa35b9023af9f9b5
kevinntech <kevinntech@nate.com>
2022-01-16 19:27:55 +0900

    Initialize #1 - Spring Boot
    스프링 부트 setup 완료

---
+++ src/test/java/me/kevinntech/gitflow/GitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kevinntech.gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
